9545807af09e0ff11069436f21cf1705ad12a539
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-10 15:02:35 +0300

    Initial commit

---
+++ Currency/Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Money
+{
+    public class Currency
+    {
+        public string Unit;
+        public string Isim;
+        public string CurrencyName;
+        public string ForexBuying;
+        public string ForexSelling;
+        public string BanknoteBuying;
+        public string BanknoteSelling;
+        public string CrossRateUS;
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Xml;
+using System.Xml.Linq;
+using Money;
+
+namespace Curr
+{
+    class Program
+    {
+
+        static readonly string URL = "https://www.tcmb.gov.tr/kurlar/today.xml";
+        static readonly string fileName = "today.xml";
+
+        static void Main(string[] args)
+        {
+            DownloadXML();
+            XDocument doc = XDocument.Load(fileName);
+
+            var res = from node in doc.Descendants("Currency")
+                      select new Currency
+                      {
+                          Unit = node.Element("Unit").Value,
+                          Isim = node.Element("Isim").Value,
+                          CurrencyName = node.Element("CurrencyName").Value,
+                          ForexBuying = node.Element("ForexBuying").Value,
+                          ForexSelling = node.Element("ForexSelling").Value,
+                          BanknoteBuying = node.Element("BanknoteBuying").Value,
+                          BanknoteSelling = node.Element("BanknoteSelling").Value,
+
+                      };
+
+            foreach (var obj in res)
+            {
+                System.Console.WriteLine(value: obj);
+            }
+        }
+
+        static void DownloadXML()
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(URL, fileName);
+            }
+        }
+    }
+}
+
