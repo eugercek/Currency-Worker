43a03d175492fd9e27ccee0d2945e9f54656d242
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-12 14:17:28 +0300

    Add string extension

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class StringParseOrDefault
+    {
+        public static T ParseOrDefault<T>(this string argumentString)
+        {
+            if (String.IsNullOrEmpty(argumentString))
+            {
+                return default(T);
+            }
+            else
+            {
+                return (T)Convert.ChangeType(argumentString, typeof(T));
+            }
+
+        }
+    }
+}
