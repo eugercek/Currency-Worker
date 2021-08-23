103edfcb6260a9ee1922d63bef10251ffb73d3a7
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-23 12:14:17 +0300

    Add systemd timer and literate setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p ~/.local/bin
+
+cd ./Program
+dotnet publish -c Release -o ~/.local/bin
+cd ..
+
+sudo cp ./currency-downloader.service /etc/systemd/system/
+sudo cp ./currency-downloader.timer /etc/systemd/system/
+
+sudo systemctl daemon-reload
+
+sudo setenforce 0
+
+sudo systemctl start currency-downloader
