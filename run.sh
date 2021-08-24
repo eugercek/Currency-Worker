d67ce72beada77e0e840507af021acdb6ec10712
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-24 10:04:30 +0300

    Add timer unit

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-sudo systemctl stop curdow
+sudo systemctl stop curdow.timer
 sudo cp /home/umut/src/staj/Currency/curdow.service /etc/systemd/system/
+sudo cp /home/umut/src/staj/Currency/curdow.timer /etc/systemd/system/
 sudo systemctl daemon-reload
+sudo systemctl start curdow.timer
--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo setenforce 0
 sudo mkdir -p /srv/curdow
 sudo rm -rf /srv/curdow/*
 sudo chown umut /srv/curdow
-sudo systemctl stop curdow
+sudo systemctl stop curdow.timer
 cd /home/umut/src/staj/Currency/Program
 dotnet publish -c Release -o /srv/curdow
